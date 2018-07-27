db71df5bfea95b5d58a6545d7397bb07ce19e8ed
jamesmiller21212 <41803611+jamesmiller21212@users.noreply.github.com>
2018-07-27 14:52:52 -0400

    Update MCP3008.py

--- MCP3008.py
+++ MCP3008.py
@@ -21,6 +21,7 @@ class MCP3008:
         self.spi.open(self.bus, self.device)
     
     def read(self, channel = 0):
+        spi.max_speed_hz=1350000
         adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
         data = ((adc[1] & 3) << 8) + adc[2]
         return data
